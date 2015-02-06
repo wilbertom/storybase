147e9d51685ee55486a24e4bdf1e383178749b85
Wilberto Morales <wilbertomorales777@gmail.com>
2015-02-05 23:19:35 -0600

    Make badges smaller and into spaned images

--- static/js/viewer/badges/views.js
+++ static/js/viewer/badges/views.js
@@ -7,9 +7,8 @@
     var Views = storybase.badges.views;
 
     var BadgeView = Views.BadgeView = Backbone.View.extend({
-        template: _.template('<div class="badge">\
-                                <img src="<%= icon_uri %>" />\
-                              </div>'),
+        tagName: 'span',
+        template: _.template('<img class="badge" src="<%= icon_uri %>" />'),
         render: function(){
             this.$el.html(this.template(this.model.attributes));
             return this;
@@ -22,7 +21,6 @@
         },
         render: function() {
             this.collection.each(function(m) {
-                console.log(this, m);
                 var badgeView = new BadgeView({model: m});
                 this.$el.append(badgeView.render().el);
 
