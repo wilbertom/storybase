5d03bd37a32cbe2a3c971b0f4f1f81009f309b45
Wilberto Morales <wilbertomorales777@gmail.com>
2015-02-10 05:07:42 -0600

    Badge permission psudo code

--- apps/storybase_badge/models.py
+++ apps/storybase_badge/models.py
@@ -8,9 +8,42 @@ Denver Foundation sponsored the story.
 """
 
 from django.db import models
+from apps.storybase.models import PermissionMixin
 
 
-class Badge(models.Model):
+class BadgePermission(PermissionMixin):
+    """
+    We don't want everyone to be able to delete and add badges. They
+    are used to aggregate content, certain people will have certain
+    badges.
+
+    """
+
+    def _can_edit(self, user):
+        """
+        We assume that if someone can add a badge to a story,
+        then they can remove it from it.
+
+        This assumption might be false some day so don't use this
+        method directly.
+
+        """
+        return user.is_staff
+
+    def can_add(self, user):
+        """
+        Can the user add the badge?
+        """
+        return self._can_edit(user)
+
+    def can_remove(self, user):
+        """
+        Can the user remove the badge?
+        """
+        return self._can_edit(user)
+
+
+class Badge(models.Model, BadgePermission):
     """
     A badge used to categorize, promote and sponsor stories.
 
--- apps/storybase_badge/tests.py
+++ apps/storybase_badge/tests.py
@@ -5,9 +5,11 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-import json
-from apps.storybase_badge.models import Badge
+from django.test import TestCase
 from tastypie.test import ResourceTestCase
+
+from apps.storybase_badge.models import Badge
+from apps.storybase_user.models import User
 
 
 class TestBadgeResource(ResourceTestCase):
@@ -25,7 +27,7 @@ class TestBadgeResource(ResourceTestCase):
     def test_getting_badges(self):
 
         resp = self.api_client.get('/api/0.1/badges/', format='json')
-        print(resp)
+
         objects = self.deserialize(resp)['objects']
 
         self.assertHttpOK(resp)
@@ -33,10 +35,35 @@ class TestBadgeResource(ResourceTestCase):
         self.assertEqual(len(objects), 2)
 
         self.assertEquals(objects[0], {
-            'pk': str(self.b1.pk),
-            'name': 'Denver Foundation',
-            'description': 'A member of the Denver Foundation sponsored this story.',
-            'icon_uri': None,
-            'resource_uri': '/api/0.1/badges/{0}/'.format(self.b1.pk)
+            u'id': self.b1.pk,
+            u'name': u'Denver Foundation',
+            u'description': u'A member of the Denver Foundation sponsored this story.',
+            u'icon_uri': u'',
+            u'resource_uri': u'/api/0.1/badges/{0}/'.format(self.b1.pk)
         })
+
+
+class TestBadgePermissions(TestCase):
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+
+        self.regular_user = User.objects.create_user('john', 'lennon@thebeatles.com', 'django-password')
+        self.regular_user.is_staff = False
+        self.regular_user.save()
+
+        self.staff_user = User.objects.create_user('matt', 'matt@floodlightproject.org', 'on-the-other-side')
+        self.staff_user.is_staff = True
+        self.staff_user.save()
+
+    def test_regular_user_badge(self):
+        b = Badge()
+        self.assertFalse(b.can_add(self.regular_user))
+        self.assertFalse(b.can_remove(self.regular_user))
+
+    def test_super_user_badge(self):
+        b = Badge()
+        self.assertTrue(b.can_add(self.staff_user))
+        self.assertTrue(b.can_remove(self.staff_user))
+
 
